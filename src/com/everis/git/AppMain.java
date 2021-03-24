7232878409c47b0ca612451cfb1c553ccb6fdbd2
jtellezr <everis.formacion.svq@gmail.com>
2021-03-24 13:48:33 +0100

    Version 1 - GIT

---
+++ src/com/everis/git/AppMain.java
@@ -0,0 +1,22 @@
+package com.everis.git;
+
+/**
+ * Formación GIT
+ * 
+ * @author everis
+ *
+ */
+public class AppMain {
+
+	/**
+	 * Método principal.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo");
+
+	}
+
+}
